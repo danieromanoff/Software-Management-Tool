425176d29bced83b8b8d58969b2925d036c0a852
Danieromanoff <dlromanoff@gmail.com>
2017-03-04 12:34:51 -0500

    Updating features

---
+++ Software-Management-Tool-feature-Stephen/SoftwareManagementTool/src/softwaremanagementtool/agile/backlogview/BacklogViewController.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package softwaremanagementtool.agile.backlogview;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import softwaremanagementtool.agile.AgileProject;
+import softwaremanagementtool.agile.BacklogEntry;
+import softwaremanagementtool.agile.DateUtil;
+import softwaremanagementtool.agile.UserStory;
+
+/**
+ * @author Stephen
+ *
+ */
+public class BacklogViewController {
+
+	private AgileProject agilePrj;
+	@FXML
+	private TableView<BacklogEntry> backlogTable;
+	@FXML
+	private TableColumn<BacklogEntry, Integer> IDColumn;
+	@FXML
+	private TableColumn<BacklogEntry, String> TitleColumn;
+  @FXML
+  private AnchorPane backlogEntryPane;
+
+	
+	@FXML
+  private void initialize() {
+    // Initialize the person table with the two columns.
+   	IDColumn.setCellValueFactory(cellData -> cellData.getValue().IDProperty().asObject());
+    TitleColumn.setCellValueFactory(cellData -> cellData.getValue().titleProperty());
+       
+    // Clear user story details.
+   // showUserStoryDetails(null);
+       
+    // Listen for selection changes and show the user story details when changed.
+    backlogTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showEntryDetails(newValue));
+    
+	}
+    
+	// showing user story 
+  private void showEntryDetails(BacklogEntry blEntry) {
+  	try {
+			agilePrj.showBacklogEntry(blEntry);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+  }
+  
+  public void setLast () {
+  	backlogTable.getSelectionModel().selectLast(); 
+  }
+  
+  public BacklogEntry getSelectedItem() {
+  	return backlogTable.getSelectionModel().getSelectedItem();
+  }
+  
+  public void setAgilePrj(AgileProject agileProject) {
+    this.agilePrj = agileProject;
+      
+    backlogTable.setItems(agilePrj.getBacklogList());
+  
+  }
+
+  public AnchorPane getBacklogEntryPane() {
+		return backlogEntryPane;
+	}
+}
