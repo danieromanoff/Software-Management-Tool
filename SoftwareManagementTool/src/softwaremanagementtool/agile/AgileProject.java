e378914c0ff51716ce8fae5f2766d7f73e4be6ff
slmatson <smatson@stevens.edu>
2017-03-11 19:51:03 -0500

    Added XML Database

--- SoftwareManagementTool/src/softwaremanagementtool/agile/AgileProject.java
+++ SoftwareManagementTool/src/softwaremanagementtool/agile/AgileProject.java
@@ -1,28 +1,29 @@
 package softwaremanagementtool.agile;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
+import javax.xml.bind.JAXBException;
+
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import softwaremanagementtool.SoftwareManagementToolMain;
 
 import softwaremanagementtool.agile.ui.BacklogUi;
 import softwaremanagementtool.agile.ui.DashboardUi;
 import softwaremanagementtool.agile.ui.ReportsUi;
 import softwaremanagementtool.agile.ui.SprintUi;
 import softwaremanagementtool.agile.UserStory;
-
-
+import softwaremanagementtool.agile.db.AgileXmlDatabase;
 
 public class AgileProject {
   
    /**
    *  Class Attributes
    */
-  private String projectName;
+  
   private BorderPane mainLayout;
   private Stage primaryStage;
   private ProductBacklog productBacklog;
@@ -35,8 +36,8 @@ public class AgileProject {
   /**
    *  Constructor
    */
-  public AgileProject(String prjName, Stage inStage, BorderPane inLayout) throws IOException {
-    projectName = prjName;
+  public AgileProject(String mode, Stage inStage, BorderPane inLayout) throws IOException {
+    
     primaryStage = inStage;
     mainLayout = inLayout;
     productBacklog = new ProductBacklog();
@@ -46,7 +47,9 @@ public class AgileProject {
     dashboardUi = new DashboardUi(this);
     backlogUi = new BacklogUi(this);
     reportsUi = new ReportsUi(this);
-    
+    if (mode.equals("OPEN")) {
+    	load();
+    }
     showDashboard();
   }
 
@@ -124,11 +127,86 @@ public class AgileProject {
   public void newSprint() throws IOException {
     Sprint newSprint = new Sprint();
     newSprint.setID(sprintList.nextId());
-    sprintList.sprintList().add(newSprint);
+    sprintList.get().add(newSprint);
     //sprintViewController.showUserSprint(newSprint); TODO
     //sprintViewController.setLastListItem();
   	
     
   }
   
+  public void save() {
+
+  	
+  	File file = new File("test.xml");
+  	
+  	AgileXmlDatabase db;
+		try {
+			db = new AgileXmlDatabase(file, this);
+			db.saveDataToFile();
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+  }
+  
+  public void load() {
+  	
+    File file = new File("test.xml");
+  	
+  	AgileXmlDatabase db;
+		try {
+			db = new AgileXmlDatabase(file, this);
+			db.loadDataFromFile();
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+  	
+  } 
+  
+  public List<UserStory> getUserStories() {
+  	ObservableList<UserStory> list = FXCollections.observableArrayList();
+  	for (int i = 0; i < getBacklogList().size(); i++) {
+  		if (getBacklogList().get(i).getType() == UserStory.type) {
+  			list.add((UserStory) getBacklogList().get(i));
+  		}
+  	}
+  	return list;
+  }
+  
+  public void loadUserStories(List<UserStory> list) {
+  	
+  	for (int i = 0; i < list.size(); i++) {
+  		getBacklogList().add(list.get(i));
+  	}
+  }
+  
+  public List<ChangeRequest> getChangeReqs() {
+  	ObservableList<ChangeRequest> list = FXCollections.observableArrayList();
+  	for (int i = 0; i < getBacklogList().size(); i++) {
+  		if (getBacklogList().get(i).getType() == ChangeRequest.type) {
+  			list.add((ChangeRequest) getBacklogList().get(i));
+  		}
+  	}
+  	
+  	return list;
+  }
+  
+  public void loadChangeReqs(List<ChangeRequest> list) {
+  	
+  	for (int i = 0; i < list.size(); i++) {
+  		getBacklogList().add(list.get(i));
+  	}
+  }
+  
+  public List<Sprint> getSprints() {
+  	return sprintList.get();
+  }
+  
+  public void loadSprints(List<Sprint> list) {
+  	
+  	for (int i = 0; i < list.size(); i++) {
+  		sprintList.add(list.get(i));
+  	}
+  }
 }  
