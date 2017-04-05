fa199407494f3e283f13afd61da77a88d8c04b94
slmatson <smatson@stevens.edu>
2017-04-04 22:15:20 -0400

    fixed run time error

--- SoftwareManagementTool/src/softwaremanagementtool/agile/dashboardview/DashboardViewController.java
+++ SoftwareManagementTool/src/softwaremanagementtool/agile/dashboardview/DashboardViewController.java
@@ -15,14 +15,6 @@ public class DashboardViewController {
   
   private AgileProject agilePrj;
   
-  @FXML
-  private LineChart lineChart;
-  
-  @FXML
- CategoryAxis xAxis = new CategoryAxis();
-  
- @FXML
- NumberAxis yAxis = new NumberAxis("Backlog", 0, 100, 10);
  
  @FXML
  private AnchorPane ChartUpperLeft;
@@ -44,42 +36,19 @@ public class DashboardViewController {
     // TBD set Name
   }
   
-  public void setData(){
-  	  	
-  	
-  	ObservableList<XYChart.Series> lineChartData = FXCollections.observableArrayList(
-        new LineChart.Series<Integer,Integer>("Series 1", FXCollections.observableArrayList(
-            new XYChart.Data("1 2/24/17", 10),
-            new XYChart.Data("2", 9),
-            new XYChart.Data("3", 8),
-            new XYChart.Data("4", 7),
-            new XYChart.Data("5", 6)
-        )),
-        new LineChart.Series<Integer,Integer>("Series 2", FXCollections.observableArrayList(
-            new XYChart.Data("4", 8),
-            new XYChart.Data("5", 7),
-            new XYChart.Data("6", 6),
-            new XYChart.Data("7", 5)
-        ))
-  
-    );
-  	lineChart.setData(lineChartData);
-  	
-   yAxis.setLabel("Backlog");
-   yAxis.setUpperBound(15);
-  	
-  	
+  public AnchorPane getChartUpperLeft() {
+  	return ChartUpperLeft;
+  }
+  public AnchorPane getChartUpperRight() {
+  	return ChartUpperRight;
+  }
+  public AnchorPane getChartLowerLeft() {
+  	return ChartLowerLeft;
+  }
+  public AnchorPane getChartLowerRight() {
+  	return ChartLowerRight;
   }
   
-  public void setLineData(LineChartData data) {
-  	lineChart.setData(data.data);
-  	
-    yAxis.setLabel(data.yAxisLabel);
-    xAxis.setLabel(data.xAxisLabel);
-    
-    lineChart.setTitle(data.title);
-    
-  }
   
   
 }
